93abba0da9a814e969871e39d0c96b6d1f524258
mikhail nikolaev <mikemein18081978@gmail.com>
2025-11-25 00:20:41 +0300

    исправил баг в mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -6,28 +6,36 @@ from masks import get_mask_account
 """В модуле преобразуется формат даты, и маскировка ромеров карт и банковских счетов"""
 
 
-def get_date(iso_date_str: str) -> str:
-    """Преобразует дату из формата ISO8601 в формат 'DD.MM.YYYY'"""
-    """Убираем формат секунды"""
-    cleaned_iso_date = iso_date_str.split('.')[0]
-    """Подставляем в метод преобразованную строку"""
-    date_obj = datetime.strptime(cleaned_iso_date, "%Y-%m-%dT%H:%M:%S")
-    """Возвращаем строку нужного формата"""
-    return date_obj.strftime("%d.%m.%Y")
+# def get_date(iso_date_str: str) -> str:
+#     """Преобразует дату из формата ISO8601 в формат 'DD.MM.YYYY'"""
+#     """Убираем формат секунды"""
+#     cleaned_iso_date = iso_date_str.split('.')[0]
+#     """Подставляем в метод преобразованную строку"""
+#     date_obj = datetime.strptime(cleaned_iso_date, "%Y-%m-%dT%H:%M:%S")
+#     """Возвращаем строку нужного формата"""
+#    # return date_obj.strftime("%d.%m.%Y")
+# print(get_date("2024-03-11T02:26:18.671407"))
 
 """Преобразование в формат маска счетов и карт"""
+
+
 def mask_account_card(account_string: str) -> str:
-    account_name = list()
+    account_name = ""
+    card_string = list()
     for i in account_string:
         if i.isalpha():
-            account_name.append("i")
-    if account_name == "Счет":
-        return f'Счет + " " + {get_mask_account(accound_stribg)}'
-    card_string = list()
-    if accound_name != "Счет":
-        for i in accound_string:
+            account_name += i
+    if account_string.startswith("Счет"):
+      return f'Счет {get_mask_account(account_string)}'
+    else:
+        for i in account_string:
             if i.isdigit():
-                card_string.append("i")
-        return account_name + " " + get_mask_card_number(card_srting)
+                card_string.append(i)
+    masked_card = get_mask_card_number(''.join(card_string))
+    return account_name + " " + masked_card
+
+#print(mask_account_card("Visa Platinum 7000792289606361"))  # Ожидаемый результат: "Visa Platinum 7000 79** **** 6361"
+#print(mask_account_card("Счет 73654108430135874305"))  # Ожидаемый результат: "Счет **4305"
+
 
 
