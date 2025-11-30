c562821c639de94400e1efd1ac4469c7566c5364
mikhail nikolaev <mikemein18081978@gmail.com>
2025-11-30 22:45:43 +0300

    написана функция filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,19 @@
+from typing import List, Dict, Any
+
+
 """Функция filter_by_state , принимает список словарей и опционально
    значение ключа state (по умолчанию 'EXECUTED'. Возвращает новый список
    словарей, содержащий только те словари, у которых ключ state соответствует
    указаному значению"""
 
 
-def filter_by_state(customer_information: str, state_1='EXECUTED') -> str:
-    pass
+def filter_by_state(dict_list: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    filtered_list = []
+    for item in dict_list:
+        if item.get('state') == state:
+            filtered_list.append(item)
+
+    return filtered_list
 
 
 """Функция sort_by_date ,принимает список словарей и необязательный параметр, 
