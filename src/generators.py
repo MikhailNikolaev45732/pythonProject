caaa1a71e184c52ede8a0398fe1a8dd9a80360bf
mikhail nikolaev <mikemein18081978@gmail.com>
2025-12-24 22:43:10 +0300

    написание кода transaction_description

--- src/generators.py
+++ src/generators.py
@@ -1,16 +1,35 @@
 import json
 
 
+# Функция возвращает итератор, который выдает транзакции, где валюта операции соответствует заданной
 def filter_by_currency(transactions, currency):
+     """Задаем перебор словарей по ключу currency"""
      transaction_usd = (transaction for transaction in transactions
      if transaction["operationAmount"]["currency"]["code"] == currency)
      return transaction_usd
 
 
 if __name__ == "__main__":
+    """Обращаемся к списоку словарей в файле transactions.txt"""
     with open('transactions.txt', 'r', encoding='utf-8') as file:
         transactions = json.load(file)
 
     usd_transactions = filter_by_currency(transactions, "USD")
     for _ in range(3):
         print(next(usd_transactions))
+
+
+# Генератор transaction_descriptions, принимает список словарей с транзакциями и возвращает описание каждой
+# операции по очереди
+def transaction_descriptions(transactions):
+    description_list = (transaction["description"] for transaction in transactions)
+    return description_list
+
+if __name__ == "__main__":
+    """Обращаемся к списоку словарей в файле transactions.txt"""
+    with open('transactions.txt', 'r', encoding='utf-8') as file:
+        transactions = json.load(file)
+
+    descriptions = transaction_descriptions(transactions)
+    for _ in range(5):
+        print(next(descriptions))
