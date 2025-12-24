c1e5b92811fbd85659517a7a329f267a625687a2
mikhail nikolaev <mikemein18081978@gmail.com>
2025-12-24 20:36:12 +0300

    написание кода filter_by_currency_2

--- src/generators.py
+++ src/generators.py
@@ -2,17 +2,15 @@ import json
 
 
 def filter_by_currency(transactions, currency):
-    with open('transactions.txt', 'r') as file:
-        transactions = json.load(file)
-        transaction_usd = (transaction for transaction in transactions
-        if transaction["operationAmount"]["currency"]["code"] == currency)
-        return transaction_usd
+     transaction_usd = (transaction for transaction in transactions
+     if transaction["operationAmount"]["currency"]["code"] == currency)
+     return transaction_usd
 
 
 if __name__ == "__main__":
-    with open('transactions.txt', 'r') as file:
+    with open('transactions.txt', 'r', encoding='utf-8') as file:
         transactions = json.load(file)
 
     usd_transactions = filter_by_currency(transactions, "USD")
-    for _ in range(5):
+    for _ in range(3):
         print(next(usd_transactions))
