fafe984f2022771b7077d46faa7f142b5d66d0e0
mikhail nikolaev <mikemein18081978@gmail.com>
2025-12-25 02:48:47 +0300

    тестирование кода filter_by_currency

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,5 +1,9 @@
 import pytest
-
+from src.generators import filter_by_currency
+import json
+#from conftest import filter_by_currency_e
+#with open('src.transactions.txt', 'r', encoding='utf-8') as file:
+   # transactions = json.load(file)
 
 @pytest.fixture
 def filter_by_currency_e():
@@ -48,7 +52,11 @@ def filter_by_currency_e():
             "from": "Visa Classic 6831982476737658",
             "to": "Visa Platinum 8990922113665229"
         }]
-    return [{
+
+
+@pytest.fixture
+def transactions():
+        return [{
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
@@ -62,8 +70,8 @@ def filter_by_currency_e():
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
             "to": "Счет 11776614605963066702"
-            },
-            {
+        },
+        {
             "id": 142264268,
             "state": "EXECUTED",
             "date": "2019-04-04T23:20:05.206878",
@@ -77,7 +85,22 @@ def filter_by_currency_e():
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
             "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
             },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
         {
             "id": 895315941,
             "state": "EXECUTED",
@@ -92,5 +115,31 @@ def filter_by_currency_e():
             "description": "Перевод с карты на карту",
             "from": "Visa Classic 6831982476737658",
             "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
         }]
+
+
+
+
+def test_filter_by_currency(transactions, filter_by_currency_e):
+     result = list(filter_by_currency(transactions, 'USD'))
+     assert result == filter_by_currency_e
+
+
+
+
 
