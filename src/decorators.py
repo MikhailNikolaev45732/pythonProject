279ce7cf8da14030005c0be6fa5bac1d02c52616
mikhail nikolaev <mikemein18081978@gmail.com>
2026-01-08 23:53:52 +0300

    Написан начальный декоратор

--- src/decorators.py
+++ src/decorators.py
@@ -1,21 +1,63 @@
+from functools import wraps
+
+
+# def log(filename=None):
+#     def decorator(func):
+#         @wraps(func)
+#         def wrapper(*args):
+#             result = func(*args)
+#             if filename:
+#                 with open(filename, 'a') as f:
+#                     f.write(f"Function: {func.__name__}. Result: {result}\n")
+#             else:
+#                 print(f"Function:{func.__name__}. Result: {result}")
+#             return result
+#         return wrapper
+#     return decorator
+#
+#
+# @log(filename="mylog.txt")
+# #@log()
+# def my_function(x, y):
+#     """Функция складывает аргументы полученные на вход"""
+#     return x + y
+#
+# my_function(1, 2)
+
+
 def log(filename=None):
+    """Декоратор для логирования"""
+
     def decorator(func):
-        def wrapper(*args):
-            result = func(*args)
-            if filename:
-                with open(filename, 'a') as f:
-                    f.write(f"Function: {func.__name__}. Result: {result}\n")
-            else:
-                print(f"Function:{func.__name__}. Result: {result}")
-            return result
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                log_message = f"Function: {func.__name__}. Result: {result}"
+                if filename:
+                    with open(filename, 'a') as f:
+                        f.write(f"Function: {func.__name__}. Result: {result}\n")
+                else:
+                    print(log_message)
+                return result
+            except Exception as e:
+                log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(log_message)
+                else:
+                    print(log_message)
+
         return wrapper
+
     return decorator
 
 
 @log(filename="mylog.txt")
-#@log()
+@log()
 def my_function(x, y):
+    """Функция складывает аргументы полученные на вход"""
     return x + y
 
 my_function(1, 2)
-
+help(my_function)
