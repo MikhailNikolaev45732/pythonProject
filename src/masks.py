fbef9c3718f92a086d228d96359ee6f0ca30f4bd
mikhail nikolaev <mikemein18081978@gmail.com>
2026-02-10 22:25:45 +0300

    отладка логгирования с записью логгов в файл masks.log

--- src/masks.py
+++ src/masks.py
@@ -1,50 +1,41 @@
-import requests
-import os
-import datetime
 import logging
-from venv import logger
-
 
 """Функция get_mask_card_number принимает на вход номер карты и возвращает
 её маску
 входной аргумент: 7000792289606361
 выход функции: 7000 79** ****6361"""
 
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format='%(levelname)s:%(name)s:%(message)s'
-# )
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    filename='../logs/masks.log',
+    encoding='utf-8',
+    filemode='w'
+)
 
-logger = logging.getLogger('masks')
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('../logs/masks.log', encoding='utf-8')
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
-file_handler.setFormatter(file_formatter)
-logger.addHandler(file_handler)
 
-# logging.basicConfig(
-#     filename='masks.log',
-#     filemode='a+',
-#     format='%(levelname)s:%(name)s:Request time: %(asctime)s',
-#     level=logging.INFO
-# )
-#
-#
-# logger = logging.getLogger()
+card_logger = logging.getLogger('mask.card.number')
+account_logger = logging.getLogger('mask.account')
 
 
 def get_mask_card_number(card_number: int) -> str:
     """Функция  принимает на вход номер карты и возвращает ее маску."""
-    logger.info("Функция получает на вход номер карты")
+    card_logger.info("Функция получает на вход номер карты")
     card_str = str(card_number)
     card_num_len = len(card_str)
 
     if card_num_len > 0 and card_num_len != 16:
+        card_logger.error("Количество знаков не соответствует заданному формату")
         raise ValueError("Некорректный ввод номера")
     elif card_num_len == 0:
+        card_logger.error("Не введены данные")
         raise ValueError("Введите номер")
 
     return card_str[:4] + " " + card_str[4:6] + "** ****" + card_str[-4:]
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number(7000792289606361))
 
 
 """Функция get_mask_account принимает на вход номер счета и возвращает
@@ -55,13 +46,19 @@ def get_mask_card_number(card_number: int) -> str:
 
 def get_mask_account(account_number: int) -> str:
     """Функция  принимает на вход номер счета и возвращает его маску."""
+    account_logger.info("Функция принимает на вход номер счета карты")
     account_str = str(account_number)
     account_num_len = len(account_str)
 
     if account_num_len > 0 and account_num_len != 20:
+        account_logger.error("Количество знаков не соответствует заданному формату")
         raise ValueError("Некорректный ввод номера")
     elif account_num_len == 0:
+        account_logger.error("Не введены данные")
         raise ValueError("Введите номер")
 
     return "**" + account_str[-4:]
 
+
+if __name__ == '__main__':
+    print(get_mask_account(73654108430135874305))
