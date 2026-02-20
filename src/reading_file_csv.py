1263da443a45f074e5e073ef313b2383eee37198
mikhail nikolaev <mikemein18081978@gmail.com>
2026-02-20 21:32:17 +0300

    Исправление ошибок в ДЗ

--- src/reading_file_csv.py
+++ src/reading_file_csv.py
@@ -1,20 +1,17 @@
 import pandas as pd
-import json
-
-transactions_reading = pd.read_csv(
-    r'https://github.com/skypro-008/transactions/raw/main/transactions.csv?plain=1',
-    delimiter=';'
-)
-pd.set_option('display.width', None)
 
 
-def get_transactions(df: pd.DataFrame) -> int:
-    d_f = df.iloc[0:]
-    json_str = d_f.to_json(orient="records", indent=4)
-    json_data = json.loads(json_str)
-    formatted_json = json.dumps(json_data, ensure_ascii=False, indent=4)
-    return formatted_json
+address_file_csv = r'https://github.com/skypro-008/transactions/raw/main/transactions.csv?plain=1'
+
+"""Функция принимает на вход путь до файла CSV и выдаёт список словарей с транзакциями"""
+
+def reading_csv(address_csv: str) -> list:
+
+    pd_file = pd.read_csv(address_csv, delimiter=';')
+    result = pd_file.to_dict(orient='records')
+
+    return result
 
 
 if __name__ == "__main__":
-    print(get_transactions(transactions_reading))
+    print(reading_csv(address_file_csv))
--- src/reading_file_excel.py
+++ src/reading_file_excel.py
@@ -1,18 +1,18 @@
 import pandas as pd
-import json
 
 
-transactions_reading = pd.read_excel(r"C:\Users\acer\Downloads\transactions_excel.xlsx")
-pd.set_option('display.width', None)
+address_file_excel = r"C:\Users\acer\Downloads\transactions_excel.xlsx"
 
+"""Функция принимает на вход путь до файла EXCEL и выдаёт список словарей с транзакциями"""
 
-def get_transactions(df: pd.DataFrame) -> int:
-    d_f = df.iloc[0:]
-    json_str = d_f.to_json(orient="records", indent=4)
-    json_data = json.loads(json_str)
-    formatted_json = json.dumps(json_data, ensure_ascii=False, indent=4)
-    return formatted_json
+def reading_excel(address_ex: str) -> list:
+    pd_file = pd.read_excel(address_ex)
+    result = pd_file.to_dict(orient='records')
+
+    return result
 
 
 if __name__ == "__main__":
-    print(get_transactions(transactions_reading))
+    print(reading_excel(address_file_excel))
+
+
