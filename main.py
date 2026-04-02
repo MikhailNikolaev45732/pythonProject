eb30e9387aaa6f2f418ffc4cd1a87519a1cea06a
mikhail nikolaev <mikemein18081978@gmail.com>
2026-04-03 00:39:11 +0300

    отладка багов

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from src.search_operations import print_sorted_category
 from src.reading_file_csv import reading_csv
 from src.reading_file_excel import reading_excel
 from src.utils import get_transaction_data
+from pathlib import Path
 
 
 def main():
@@ -23,9 +24,20 @@ def main():
         2. Получить информацию о транзакциях из CSV-файла
         3. Получить информацию о транзакциях из XLSX-файла'''
     )
+    #path = str(Path(r"C:/Users/acer/Downloads/transactions_excel.xlsx").expanduser())
+    # new_path = path / "new_folder"
+    #path_1 = str(Path(r'https://github.com/skypro-008/transactions/raw/main/transactions.csv?plain=1').expanduser())
+    # new_path_1 = path_1 / "new_folder"
+    #path_2 = str(Path(r'C:/Users/acer/exampl/pythonProject/data/operations.json').expanduser())
+    # new_path_2 = path_2 / "new_folder"
+
     result_exe = reading_excel(r"C:/Users/acer/Downloads/transactions_excel.xlsx")
     result_csv = reading_csv(r'https://github.com/skypro-008/transactions/raw/main/transactions.csv?plain=1')
     result_json = get_transaction_data(r'C:/Users/acer/exampl/pythonProject/data/operations.json')
+
+    #result_exe = reading_excel(path)
+    #result_csv = reading_csv(path_1)
+    #result_json = get_transaction_data(path_2)
 
     user_choic_f = int(input('Введите номер операции: '))
     print(
